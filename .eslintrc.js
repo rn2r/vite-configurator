71249caa167350f323404fba63d4f8c1d868b825
Paul Ostashkin <49794166+rn2r@users.noreply.github.com>
2023-10-07 14:16:34 +0000

    chore: added jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      jest: true,
-    },
-    plugins: ['@typescript-eslint'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: './tsconfig.json',
-    },
-    extends: [
-      'airbnb-base',
-      'airbnb-typescript/base',
-      'prettier',
-    ],
-    ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js", "commitlint.config.js", "lint-staged.config.js", "prettier.config.js"],
-    rules: {
-      "import/prefer-default-export": 0,
-    },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.eslintrc.js',
+    'commitlint.config.js',
+    'lint-staged.config.js',
+    'prettier.config.js',
+    'jest.config.js',
+  ],
+  rules: {
+    'import/prefer-default-export': 0,
+  },
 };
